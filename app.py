c0a0e041b5cc87b8e5b0f4460b4a53609134460e
StarkArk <91919606+StarkArk@users.noreply.github.com>
2023-11-18 21:36:28 -0800

    final

--- app.py
+++ app.py
@@ -53,66 +53,26 @@ def welcome():
         f"/api/v1.0/temp/<start><br/>"
         f"/api/v1.0/temp/<start>/<end>"
     )
-# @app.route("/api/v1.0/precipitationa")
-# def precipitation():
-    
-#     # Query dates and precipitation
-#     session = Session(engine)
-#     results = session.query(Measurement.date, Measurement.prcp).all()
-
-#     # Create a dictionary using 'date' as the key and 'prcp' as the value
-#     prcp_list = []
-
-#     for date, prcp in results:
-#         prcp_dict = {}
-#         prcp_dict[date] = prcp
-#         prcp_list.append(prcp_dict) 
-
-#     session.close()
-
-#     # Return the list of dates and precipitation
-#     return jsonify(prcp_list)
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    # """Shows the precipitation data from 8-23-2016 
-    #    to 8-23-2027 """
-    # # return "hello"
-    # # session = Session(engine)
+    """Shows the precipitation data from 8-23-2016 
+       to 8-23-2027 """
 
-    # # # Calculates dates for the last 12 months in the data
-    # # recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
-    # # recent_year = int(recent_date[0][0:4])
-    # # recent_month = int(recent_date[0][5:7])
-    # # recent_day = int(recent_date[0][8:10])
-    # # last_date = dt.date(recent_year, recent_month, recent_day)
+    session = Session(engine)
+
     year_back = dt.date(2017, 8, 23) - dt.timedelta(days=365)
 
-    # # Query last 12 months of precipitation
+    # Query last 12 months of precipitation
     results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= year_back).all()
 
-    # # Save Query as a dictionary
-    # # prcp_ls_dict = [{"Date": prcp[0], "Precipitation": prcp[1]} for prcp in prcp_query]
+    # Save Query as a dictionary
     prcp_dict = {Date: prcp for Date, prcp in results}
-    
-    # prcp_ls_dict = {}
 
     session.close()
 
     return jsonify(prcp_dict)
-    # return "hello"
 
-    # prcp_list = []
-
-    # for date, prcp in results:
-    #     prcp_dict = {}
-    #     prcp_dict[date] = prcp
-    #     prcp_list.append(prcp_dict) 
-
-    # session.close()
-
-    # # Return the list of dates and precipitation
-    # return jsonify(prcp_list)
 
 @app.route("/api/v1.0/stations")
 def stations():
@@ -149,14 +109,17 @@ def temp_observations():
                                   filter(Measurement.station == top_station).\
                                   filter(Measurement.date >= year_back).\
                                   all()
+    
     # List of dicts containing dates and temps for the previous 12 months
     temps_list = [{Date: temp} for Date, temp in top_station_temps]
-    # Prepend station name(id)
+    
+    # Prepend top station name(id)
     temps_list = [top_station] + temps_list
 
     session.close()
 
     return jsonify(temps_list)
+
 
 @app.route("/api/v1.0/temp/<start>")
 def start_date(start):
@@ -173,15 +136,19 @@ def start_date(start):
         agg_temps = session.query(*sel).filter(Measurement.date >= start).all()[0]
         tmin, tmax, tavg = agg_temps[0], agg_temps[1], agg_temps[2],
 
+        session.close()
+
         return (
             f"Min Temp: {tmin}</br>"
             f"Max Temp: {tmax}</br>"
             f"Avg Temp: {round(tavg, 1)}"
         )
     else:
+        
+        session.close()
+
         return f"Date entered is not in the data, please use a date between {first_date} and {last_date}"
 
-    session.close()
 
 @app.route("/api/v1.0/temp/<start>/<end>")
 def date_range(start, end):
@@ -199,15 +166,19 @@ def date_range(start, end):
                                         filter(Measurement.date <= end)[0]
         tmin, tmax, tavg = agg_temps[0], agg_temps[1], agg_temps[2],
 
+        session.close()
+
         return (
             f"Min Temp: {tmin}</br>"
             f"Max Temp: {tmax}</br>"
             f"Avg Temp: {round(tavg, 1)}"
         )
     else:
+
+        session.close()
+
         return f"Start and end date range entered is not in the data, please use dates between {first_date} and {last_date}"
 
-    session.close()
 
 if __name__ == "__main__":
     app.run(debug=True)
